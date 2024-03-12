7e9ce2841cbc2e45c7fd0ff12eb3a5f61d55c582
Mukizar5 <mukizar5@gmail.com>
2024-03-12 21:52:31 +0300

    Add project files.

---
+++ Calculating School Fees Payment/enum/Grade.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Calculating_School_Fees_Payment.@enum
+{
+    public enum GradeKabojja
+{
+        grade1=1,
+        grade2=2,
+        grade3=3,
+        grade4=4,
+        grade5=5,
+        grade6=6,
+        grade7=7,
+        grade8=8
+}
+
+
+public enum GradeMartys
+{
+    grade1 = 1,
+    grade2 = 2,
+    grade3 = 3,
+    grade4 = 4,
+    grade5 = 5,
+    grade6 = 6,
+    grade7 = 7,
+    grade8 = 8
+}
+
+public enum GradeKampala
+{
+    grade1 = 1,
+    grade2 = 2,
+    grade3 = 3,
+    grade4 = 4,
+    grade5 = 5,
+    grade6 = 6,
+    grade7 = 7,
+    grade8 = 8
+}
+
+}
---
+++ Calculating School Fees Payment/enum/Schools.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculating_School_Fees_Payment.@enum
+{
+    public enum Schools
+    {
+        kabojjaJuniorSchool = 0,
+        ugandaMartyrsSchool = 1,
+        kampalaParentsSchool = 2
+
+    }
+}
